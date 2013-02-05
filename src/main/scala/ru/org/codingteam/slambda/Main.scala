40286ad8cd34a0181bf8c4f9924895708d1861b5
ForNeVeR <neverthness@gmail.com>
2013-02-05 22:57:42 +0700

    Main: added a test for the CallByName reduction.

--- src/main/scala/ru/org/codingteam/slambda/Main.scala
+++ src/main/scala/ru/org/codingteam/slambda/Main.scala
@@ -3,12 +3,19 @@ package ru.org.codingteam.slambda
 import terms._
 
 object Main extends App {
- 	val tru = Lambda('t', Lambda('f', Variable('t')))
+  val id = Lambda('x', Variable('x'))
+  val tru = Lambda('t', Lambda('f', Variable('t')))
   val fls = Lambda('t', Lambda('f', Variable('f')))
 
   val and = Lambda('b', Lambda('c', Application(Application(Variable('b'), Variable('c')), fls)))
 
   println(Full.eval(Application(Application(tru, Variable('a')), Variable('b'))))
-  println(CallByName.eval(Application(Application(and, tru), tru)))
-  println(CallByName.eval(Application(Application(and, tru), tru)) == tru)
+
+  val test = Application(
+    id,
+    Application(
+      id,
+      Lambda('z', Application(id, Variable('z')))))
+  println("Call by name:")
+  println(CallByName.eval(test))
 }
